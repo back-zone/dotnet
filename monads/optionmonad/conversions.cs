07bcf359be17a8dd7d3242fb0b48e07e44ae218e
Murat YavuzKAPLAN <m@back.zone>
2024-07-29 16:23:47 +0300

    - Option conversions for IO implemented

--- monads/optionmonad/conversions.cs
+++ monads/optionmonad/conversions.cs
@@ -1,5 +1,22 @@
+using monads.iomonad;
+
 namespace monads.optionmonad;
 
 public static class conversions
 {
+    public static IO<A> asIO<A>(this Option<A> option)
+    {
+        return option.fold(
+            io.fail<A>("Option is None"),
+            io.succeed
+        );
+    }
+
+    public static async Task<IO<A>> asIOAsync<A>(this Task<Option<A>> optionTask)
+    {
+        return await optionTask.foldAsync(
+            io.fail<A>("Option is None"),
+            async a => await Task.FromResult(io.succeed(a))
+        );
+    }
 }
