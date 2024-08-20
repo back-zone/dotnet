2f01359dbc222263862099b47fafcc9397ed19c7
Murat YavuzKAPLAN <m@back.zone>
2024-08-20 17:34:08 +0300

    - Default options for JsonSerialization added

---
+++ core/Serde/Json/BackzoneJsonSerializationOptions.cs
@@ -0,0 +1,23 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace back.zone.core.Serde.Json;
+
+/// <summary>
+///     Provides configurations for JSON serialization used in the monadic framework.
+/// </summary>
+public class BackzoneJsonSerializationOptions
+{
+    /// <summary>
+    ///     A pre-configured instance of <see cref="Options" /> with specific settings for monadic serialization.
+    /// </summary>
+    public static readonly JsonSerializerOptions Options = new()
+    {
+        WriteIndented = false,
+        PropertyNameCaseInsensitive = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        NumberHandling = JsonNumberHandling.AllowReadingFromString,
+        Converters = { new OptionJsonConverterFactory() }
+    };
+}
