75aa69960e59caf1b5d807ac9d2148dbbfef88eb
Murat YavuzKAPLAN <m@back.zone>
2024-08-24 01:30:58 +0300

    - Documentation added - RedisService now runs on safe constructs - Version increased

--- storage.redis/Services/RedisService.cs
+++ storage.redis/Services/RedisService.cs
@@ -1,45 +1,102 @@
+using back.zone.core.Monads.TryMonad;
 using back.zone.storage.redis.Configuration;
 using StackExchange.Redis;
 
 namespace back.zone.storage.redis.Services;
 
+/// <summary>
+///     Represents a service for interacting with a Redis database.
+/// </summary>
 public sealed class RedisService
 {
     private readonly ConfigurationOptions _configuration;
-    private ConnectionMultiplexer _connectionMultiplexer;
+    private readonly ConnectionMultiplexer _connectionMultiplexer;
 
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="RedisService" /> class.
+    /// </summary>
+    /// <param name="redisConfiguration">The configuration for the Redis service.</param>
     public RedisService(RedisConfiguration redisConfiguration)
     {
         _configuration = redisConfiguration.AsConfigurationOptions();
         _connectionMultiplexer = ConnectionMultiplexer.Connect(_configuration);
     }
 
+    /// <summary>
+    ///     Gets a value indicating whether the connection to the Redis server is established.
+    /// </summary>
     public bool IsConnected => _connectionMultiplexer.IsConnected;
 
-    public IDatabase GetDatabase()
+    /// <summary>
+    ///     Attempts to establish a connection to the Redis server.
+    /// </summary>
+    /// <returns>A <see cref="Try{IDatabase}" /> representing the result of the connection attempt.</returns>
+    public Try<IDatabase> Connect()
     {
-        if (IsConnected) return _connectionMultiplexer.GetDatabase();
+        return Try
+            .Succeed(_connectionMultiplexer)
+            .Zip(Try.Succeed(_configuration))
+            .UnZip(DoConnect);
 
-        if (Connect()) return _connectionMultiplexer.GetDatabase();
+        static IDatabase DoConnect(
+            ConnectionMultiplexer multiplexer,
+            ConfigurationOptions options
+        )
+        {
+            if (multiplexer.IsConnected) return multiplexer.GetDatabase();
 
-        throw new InvalidOperationException("Failed to connect to Redis");
+            multiplexer = ConnectionMultiplexer.Connect(options);
+
+            return multiplexer.GetDatabase();
+        }
     }
 
-    public async Task<bool> ConnectAsync()
+    /// <summary>
+    ///     Asynchronously attempts to establish a connection to the Redis server.
+    /// </summary>
+    /// <returns>A <see cref="Try{IDatabase}" /> representing the result of the asynchronous connection attempt.</returns>
+    public async Task<Try<IDatabase>> ConnectAsync()
     {
-        if (IsConnected) return true;
+        return await Try
+            .Succeed(_connectionMultiplexer)
+            .Zip(Try.Succeed(_configuration))
+            .UnZipAsync(DoConnectAsync);
 
-        _connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(_configuration);
+        static async Task<IDatabase> DoConnectAsync(
+            ConnectionMultiplexer multiplexer,
+            ConfigurationOptions options)
+        {
+            if (multiplexer.IsConnected) return multiplexer.GetDatabase();
 
-        return IsConnected;
+            multiplexer = await ConnectionMultiplexer.ConnectAsync(options);
+
+            return multiplexer.GetDatabase();
+        }
     }
 
-    public bool Connect()
+    /// <summary>
+    ///     Gets the Redis database instance.
+    ///     If the connection is not established, it will attempt to connect first.
+    /// </summary>
+    /// <returns>A <see cref="Try{IDatabase}" /> representing the result of the database retrieval.</returns>
+    public Try<IDatabase> GetDatabase()
     {
-        if (IsConnected) return true;
+        return Try
+            .Succeed(_connectionMultiplexer)
+            .FlatMap(mp => Try.Succeed(mp.GetDatabase()))
+            .OrElse(Connect());
+    }
 
-        _connectionMultiplexer = ConnectionMultiplexer.Connect(_configuration);
-
-        return IsConnected;
+    /// <summary>
+    ///     Asynchronously gets the Redis database instance.
+    ///     If the connection is not established, it will attempt to connect first.
+    /// </summary>
+    /// <returns>A <see cref="Try{IDatabase}" /> representing the result of the asynchronous database retrieval.</returns>
+    public async Task<Try<IDatabase>> GetDatabaseAsync()
+    {
+        return await Try
+            .Succeed(_connectionMultiplexer)
+            .FlatMap(mp => Try.Succeed(mp.GetDatabase()))
+            .OrElseAsync(ConnectAsync());
     }
 }
