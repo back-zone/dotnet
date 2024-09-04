2ac2aa7be6f2baf54a9df1599aca536cec4c0e51
Murat YavuzKAPLAN <m@back.zone>
2024-09-04 23:26:27 +0300

    - Scalar and non-scalar function adjustments - Version bump

--- storage.sqlite/Services/SqliteServiceExtensions.cs
+++ storage.sqlite/Services/SqliteServiceExtensions.cs
@@ -7,6 +7,105 @@ namespace back.zone.storage.sqlite.Services;
 
 public static class SqliteServiceExtensions
 {
+    public static Try<TA> RunQuery<TA>(
+        this Try<SqliteConnection> connection,
+        Continuation<SqliteConnection, TA> continuation)
+        where TA : notnull
+    {
+        try
+        {
+            return connection.TryGetValue(out var conn)
+                ? continuation(conn)
+                : connection.TryGetException(out var exception)
+                    ? exception
+                    : new Exception("#failed_to_acquire_connection#");
+        }
+        catch (Exception e)
+        {
+            return e;
+        }
+        finally
+        {
+            if (connection.TryGetValue(out var conn)) SqliteServiceProvider.Get().ReleaseReadConnection(conn);
+        }
+    }
+
+    public static async Task<Try<TA>> RunQueryAsync<TA>(
+        this Try<SqliteConnection> connection,
+        Continuation<SqliteConnection, Task<TA>> continuation
+    )
+        where TA : notnull
+    {
+        try
+        {
+            return connection.TryGetValue(out var conn)
+                ? await continuation(conn).ConfigureAwait(false)
+                : connection.TryGetException(out var exception)
+                    ? exception
+                    : new Exception("#failed_to_acquire_connection#");
+        }
+        catch (Exception e)
+        {
+            return e;
+        }
+        finally
+        {
+            if (connection.TryGetValue(out var conn)) SqliteServiceProvider.Get().ReleaseReadConnection(conn);
+        }
+    }
+
+    public static async Task<Try<TA>> RunQueryAsync<TA>(
+        this Task<Try<SqliteConnection>> connectionAsync,
+        Continuation<SqliteConnection, Task<TA>> continuation
+    )
+        where TA : notnull
+    {
+        try
+        {
+            var connection = await connectionAsync.ConfigureAwait(false);
+            return connection.TryGetValue(out var conn)
+                ? await continuation(conn).ConfigureAwait(false)
+                : connection.TryGetException(out var exception)
+                    ? exception
+                    : new Exception("#failed_to_acquire_connection#");
+        }
+        catch (Exception e)
+        {
+            return e;
+        }
+        finally
+        {
+            if ((await connectionAsync.ConfigureAwait(false)).TryGetValue(out var conn))
+                SqliteServiceProvider.Get().ReleaseReadConnection(conn);
+        }
+    }
+
+    public static async Task<Try<TA>> RunQueryAsync<TA>(
+        this Task<Try<SqliteConnection>> connectionAsync,
+        Continuation<SqliteConnection, TA> continuation
+    )
+        where TA : notnull
+    {
+        try
+        {
+            var connection = await connectionAsync.ConfigureAwait(false);
+            return connection.TryGetValue(out var conn)
+                ? continuation(conn)
+                : connection.TryGetException(out var exception)
+                    ? exception
+                    : new Exception("#failed_to_acquire_connection#");
+        }
+        catch (Exception e)
+        {
+            return e;
+        }
+        finally
+        {
+            if ((await connectionAsync.ConfigureAwait(false)).TryGetValue(out var conn))
+                SqliteServiceProvider.Get().ReleaseReadConnection(conn);
+        }
+    }
+
     public static Try<TA> RunQuery<TA, TP>(
         this Try<(TP parameters, SqliteConnection connection)> tuple,
         Zipper<TP, SqliteConnection, TA> continuation
@@ -32,6 +131,30 @@ public static class SqliteServiceExtensions
     }
 
     public static async Task<Try<TA>> RunQueryAsync<TA, TP>(
+        this Try<(TP parameters, SqliteConnection connection)> tuple,
+        Zipper<TP, SqliteConnection, Task<TA>> continuation
+    )
+        where TA : notnull
+        where TP : notnull
+    {
+        try
+        {
+            return tuple.TryGetValue(out var flattenedTuple)
+                ? await continuation(
+                    flattenedTuple.parameters,
+                    flattenedTuple.connection
+                ).ConfigureAwait(false)
+                : tuple.TryGetException(out var exception)
+                    ? exception
+                    : new Exception("#failed_to_acquire_connection#");
+        }
+        catch (Exception e)
+        {
+            return e;
+        }
+    }
+
+    public static async Task<Try<TA>> RunQueryAsync<TA, TP>(
         this Task<Try<(TP parameters, SqliteConnection connection)>> asyncTuple,
         Zipper<TP, SqliteConnection, Task<TA>> continuation
     )
@@ -45,7 +168,34 @@ public static class SqliteServiceExtensions
             return tuple.TryGetValue(out var flattenedTuple)
                 ? await continuation(
                     flattenedTuple.parameters,
-                    flattenedTuple.connection)
+                    flattenedTuple.connection
+                ).ConfigureAwait(false)
+                : tuple.TryGetException(out var exception)
+                    ? exception
+                    : new Exception("#failed_to_acquire_connection#");
+        }
+        catch (Exception e)
+        {
+            return e;
+        }
+    }
+
+    public static async Task<Try<TA>> RunQueryAsync<TA, TP>(
+        this Task<Try<(TP parameters, SqliteConnection connection)>> asyncTuple,
+        Zipper<TP, SqliteConnection, TA> continuation
+    )
+        where TA : notnull
+        where TP : notnull
+    {
+        try
+        {
+            var tuple = await asyncTuple.ConfigureAwait(false);
+
+            return tuple.TryGetValue(out var flattenedTuple)
+                ? continuation(
+                    flattenedTuple.parameters,
+                    flattenedTuple.connection
+                )
                 : tuple.TryGetException(out var exception)
                     ? exception
                     : new Exception("#failed_to_acquire_connection#");
